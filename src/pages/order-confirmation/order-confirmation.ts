837bcae25483fe72f0eafd9101139b31e3d05bfe
Leonardo <leonardo.hrjesus@hotmail.com>
2019-07-18 21:02:08 -0300

    Salvando Pedido e informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart-service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/Pedido.service';
 
 
 
@@ -20,13 +21,15 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente : ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido : string;
 
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public cartService : CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
 
       this.pedido = this.navParams.get('pedido');
   }
@@ -51,6 +54,32 @@ export class OrderConfirmationPage {
 
   total(){
     return this.cartService.total();
+  }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
+  checkout(){
+     this.pedidoService.insert(this.pedido)
+     .subscribe(response =>{
+       this.cartService.createOrClearCart();
+       this.codPedido = this.extractId(response.headers.get('location'));
+     },
+     error =>{
+       if(error.status == 403){
+         this.navCtrl.setRoot('HomePage');
+       }
+     });
+  }
+
+  private extractId(location: string): string{
+    let position = location.lastIndexOf('/');
+    return location.substring(position +1, location.length);
   }
 
 }
